OpenMeridian105/GZDoomBuilder_M59

commit 44200f271d4f661506b885f16961e6f270f70a0e

Fixed, Map Analysis mode, "Check stuck things" check: rewritten parts of the flags checking logic to allow more accurate flag checks. Fixed, Map Analysis mode: fixed a crash when trying to dissolve an invalid sector when one of it's linedefs referenced it on the both sides. Fixed, Sectors mode: fixed incorrect undo description when deleting sectors. Internal: joined declaration and assignment of some more variables.
git-svn-id: http://svn.code.sf.net/p/doombuilder/code/branches/GZDoomBuilder@2455 e0d998f2-2e9b-42fe-843d-47128df60a06

Source/Core/Rendering/TextLabel.cs
@@ -138,22 +138,18 @@ namespace CodeImp.DoomBuilder.Rendering
 		// This updates the text if needed
 		internal void Update(float translatex, float translatey, float scalex, float scaley)
 		{
-			//FlatVertex[] verts;
-			RectangleF absview;
-			float beginx = 0;
-			float beginy = 0;
-			//bool colorcode = false;
-			//int characters = 0;
-			byte[] textbytes;
-			DataStream stream;
-
 			// Check if transformation changed and needs to be updated
 			if(transformcoords)
 			{
-				if((translatex != lasttranslatex) ||
-				   (translatey != lasttranslatey) ||
-				   (scalex != lastscalex) ||
-				   (scaley != lastscaley)) updateneeded = true;
+				if(translatex != lasttranslatex || translatey != lasttranslatey ||
+				   scalex != lastscalex || scaley != lastscaley)
+				{
+					translatex = lasttranslatex; //mxd
+					translatey = lasttranslatey; //mxd
+					scalex = lastscalex; //mxd
+					scaley = lastscaley; //mxd
+					updateneeded = true;
+				}
 			}
 			
 			// Update if needed
@@ -179,6 +175,7 @@ namespace CodeImp.DoomBuilder.Rendering
 					}
 					
 					// Transform?
+					RectangleF absview;
 					if(transformcoords)
 					{
 						// Calculate absolute coordinates
@@ -198,6 +195,7 @@ namespace CodeImp.DoomBuilder.Rendering
 					size = General.Map.Graphics.Font.GetTextSize(text, scale);
 
 					// Align the text horizontally
+					float beginx = 0;
 					switch(alignx)
 					{
 						case TextAlignmentX.Left: beginx = absview.X; break;
@@ -206,6 +204,7 @@ namespace CodeImp.DoomBuilder.Rendering
 					}
 
 					// Align the text vertically
+					float beginy = 0;
 					switch(aligny)
 					{
 						case TextAlignmentY.Top: beginy = absview.Y; break;
@@ -214,10 +213,10 @@ namespace CodeImp.DoomBuilder.Rendering
 					}
 
 					// Get the ASCII bytes for the text
-					textbytes = Encoding.ASCII.GetBytes(text);
+					byte[] textbytes = Encoding.ASCII.GetBytes(text);
 
 					// Lock the buffer
-					stream = textbuffer.Lock(0, capacity * 12 * FlatVertex.Stride,
+					DataStream stream = textbuffer.Lock(0, capacity * 12 * FlatVertex.Stride,
 									LockFlags.Discard | LockFlags.NoSystemLock);
 					
 					// Go for all chars in text to create the backgrounds

commit 0e2c8e814ee109e4b6b3b641fca149912f87c054

Fixed, Classic modes: text labels positioning logic was accidentally broken in R2455.
git-svn-id: http://svn.code.sf.net/p/doombuilder/code/branches/GZDoomBuilder@2456 e0d998f2-2e9b-42fe-843d-47128df60a06

Source/Core/Rendering/TextLabel.cs
@@ -144,10 +144,10 @@ namespace CodeImp.DoomBuilder.Rendering
 				if(translatex != lasttranslatex || translatey != lasttranslatey ||
 				   scalex != lastscalex || scaley != lastscaley)
 				{
-					translatex = lasttranslatex; //mxd
-					translatey = lasttranslatey; //mxd
-					scalex = lastscalex; //mxd
-					scaley = lastscaley; //mxd
+					lasttranslatex = translatex; //mxd
+					lasttranslatey = translatey; //mxd
+					lastscalex = scalex; //mxd
+					lastscaley = scaley; //mxd
 					updateneeded = true;
 				}
 			}