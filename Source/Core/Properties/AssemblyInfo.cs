73201373be4d2f38ddaa24d26f4706823f0a3950
skittles1 <tmx883@gmail.com>
2016-07-02 02:59:16 +0800

    Increment assembly version.

--- Source/Core/Properties/AssemblyInfo.cs
+++ Source/Core/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.0.2665")]
+[assembly: AssemblyVersion("2.3.0.2666")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
